7176863f7b6f1786e2c343c065c9c0abae325153
ownssec <ownssec@gmail.com>
2025-05-20 18:06:42 +0800

    added login axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import { useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  const loginTest = () => {
+  const apiUrl = "http://localhost:8001/api/v1";
+
+  const loginTest = async () => {
     setCount((count) => count + 1);
+    const setdata = {
+      email: "asd@gmail.com",
+      password: "e",
+    };
+    await axios
+      .post(`${apiUrl}/auth/signIn`, setdata)
+      .then((res) => {
+        console.log("DEBUGPRINT[87]: App.tsx:12: res=", res);
+        return res;
+      })
+      .catch((err) => {
+        console.log("DEBUGPRINT[88]: App.tsx:22: err=", err);
+        return err;
+      });
   };
 
   return (
